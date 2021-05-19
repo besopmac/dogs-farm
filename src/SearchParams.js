4b385ae99d2ce5fb19d279d9d1459f38207ae6ca
Bruno N. Campos <bruno@mambowifi.com>
2021-05-18 22:55:24 -0300

    Basic CSS

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
 import Pet from './Pet';
 
+const ANIMALS = ['bird', 'cat', 'dog', 'fish', 'giraffe']
+
 const SearchParams = () => {
+  const [location, setLocation] = useState('');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const [pets, setPets] = useState([]);
+ 
+  // `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+
   return (
-    <Pet
-      name="Barney"
-      animal="dog"
-      breed="Beagle"
-    />
+    <div>
+      <form>
+        <label htmlFor="location">
+          <input
+            id="location"
+            value={location}
+          /> 
+        </label>
+
+        <label htmlFor="animal">
+          <select
+            id="animal"
+            value={animal}
+          >
+            <option />
+            {
+              ANIMALS.map(animal => (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              ))
+            }
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          <select id="breed">
+            <option />
+          </select>
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
